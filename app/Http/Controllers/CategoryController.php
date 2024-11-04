ce424c1cb393175cbea5a55b8b16c10d351ae43d
unknown <dodiahmad724@gmail.com>
2024-11-04 11:31:33 +0700

    first commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function create(Request $request)
+        Category::create(attributes: [
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+}
